1064120ea03bad446ae6f7ef60bb0f00d24ff87b
gokmen.yilmaz <gokmen1977@gmail.com>
2023-01-08 14:56:10 +0300

    Add project files.

---
+++ WpfApp1/MainViewModel.cs
@@ -0,0 +1,93 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public  class MainViewModel: ViewModelBase
+    {
+
+        IDialogService DialogService { get { return GetService<IDialogService>(); } }
+
+
+        RegistrationViewModel registrationViewModel;
+
+        IMessageBoxService MessageBoxService { get { return GetService<IMessageBoxService>(); } }
+  
+
+        public MainViewModel()
+        {
+            registrationViewModel = new RegistrationViewModel();
+        }
+
+
+        [Command]
+        public void ShowRegistrationForm()
+        {
+
+            UICommand registerCommand = new UICommand(
+               id: null,
+               caption: "Register",
+               command: new DelegateCommand<CancelEventArgs>(
+                   cancelArgs => {
+                       try
+                       {
+                          
+                       }
+                       catch (Exception e)
+                       {
+                           MessageBoxService.ShowMessage(e.Message, "Error", MessageButton.OK);
+                           cancelArgs.Cancel = true;
+                       }
+                   },
+                   cancelArgs => !string.IsNullOrEmpty(registrationViewModel.UserName)
+               ),
+               isDefault: true,
+               isCancel: false
+           );
+
+            UICommand cancelCommand = new UICommand(
+                id: MessageBoxResult.Cancel,
+                caption: "Cancel",
+                command: null,
+                isDefault: false,
+                isCancel: true
+            );
+
+
+
+
+            //UICommand result = DialogService.ShowDialog(
+            //    dialogCommands: new List<UICommand>() { registerCommand, cancelCommand },
+            //    title: "Registration Dialog",
+            //    viewModel: registrationViewModel
+            //);
+
+
+
+
+            var result = DialogService.ShowDialog(
+                dialogCommands: new List<UICommand>() { registerCommand, cancelCommand },
+                title: "Child View",
+                documentType: "RegistrationView",
+                viewModel: registrationViewModel
+
+            );
+
+            var x = result;
+
+
+            //MessageBoxService.ShowMessage("denemem");
+
+
+        }
+
+    }
+}
---
+++ WpfApp1/RegistrationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class RegistrationViewModel : ViewModelBase
+    {
+        IMessageBoxService MessageBoxService { get { return GetService<IMessageBoxService>(); } }
+        public RegistrationViewModel()
+        {
+
+        }
+
+
+        [Command]
+        public void Sec()
+        {
+
+        }
+        public string UserName
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+    }
+}
